e930f0510331164d716911b2baa4ba54f325bbab
CHerdle13 <craigherdle2016@u.northwestern.edu>
2015-04-15 19:38:51 -0500

    Add acceleration bars to screen in HW5

--- HW5.X/HW5main.c
+++ HW5.X/HW5main.c
@@ -15,6 +15,45 @@
 /*
  * 
  */
+void write_accel_bars(short accels[3]){
+    int xbar;
+    int ybar;
+    int startrow = 32;
+    int startcol = 64;
+    xbar = accels[0]*10;
+    ybar = accels[1]*10;
+    int i = 0;
+    int j = 0;
+    if (xbar > 0){
+        for (i; i < xbar; i++){
+            display_pixel_set(startrow+1,startcol+i,1);
+            display_pixel_set(startrow,startcol+i,1);
+            display_pixel_set(startrow-1,startcol+i,1);
+        }
+    }
+    else{
+        for (i; i > xbar; i--){
+            display_pixel_set(startrow+1,startcol+i,1);
+            display_pixel_set(startrow,startcol+i,1);
+            display_pixel_set(startrow-1,startcol+i,1);
+        }
+    }
+    if (ybar > 0){
+        for (j; j < ybar; j++){
+            display_pixel_set(startrow+j,startcol+1,1);
+            display_pixel_set(startrow+j,startcol,1);
+            display_pixel_set(startrow+j,startcol-1,1);
+        }
+    }
+    else{
+        for (j; j > ybar; j--){
+            display_pixel_set(startrow+j,startcol+1,1);
+            display_pixel_set(startrow+j,startcol,1);
+            display_pixel_set(startrow+j,startcol-1,1);
+        }
+    }
+}
+
 void main() {
 
     acc_setup();
@@ -29,7 +68,16 @@ void main() {
 
     char message[20];
     sprintf(message, "x: %d y: %d z: %d temp: %d", accels[0], accels[1], accels[2], temp);
-    write_to_screen(message, 32, 2);
-    while(1){;};
+    write_to_screen(message, 7, 2);
+    write_accel_bars(accels);
+    int go = 0;
+    while(1){
+        if (go >= 100){
+            write_accel_bars(accels);
+            go = 0;
+        }
+        go += 1;
+    }
 }
+
 
